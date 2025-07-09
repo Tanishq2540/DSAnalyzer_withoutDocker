d70b000742ef41f74a4f1a411289a3f7b24360ac
Tanishq2540 <gtanishq25@gmail.com>
2025-07-09 18:05:13 +0530

    Prompt update

--- agents/problem_solver.py
+++ agents/problem_solver.py
@@ -17,26 +17,16 @@ def get_problem_solver_agent():
             system_message="""
                 You are a problem solver agent that is an expert in solving DSA problems.
                 You will be working with code executor agent to execute code.
-                You will be given a task and you should. 
+                You will be given a coding task and you should return a python code solving that problem.
+                If the task is not related to coding, respond only with "Kindly ask a coding related problem", after this mention "STOP" to end the execution. 
                 At the beginning of your response you have to specify your plan to solve the task.
                 Then you should give the code in a code block.(Python)
                 You should write code in a one code block at a time and then pass it to code executor agent to execute it.
                 Make sure that we have atleast 3 test cases for the code you write.
-                Once the code is executed and if the same has been done successfully, you have the results.
-                You should explain the code execution result.
-
-                Once the code and explanation is done, you should ask the code executor agent to save the code in a file.
-                like this:
-                ```python
-                code = '''
-                    print("Hello World")
-                '''
-                with open('solution.py', 'w') as f:
-                    f.write(code)
-                    print("Code saved successfully in solution.py")
-                ```
-                You should send the above code block to the code executor agent so that it can save the code in a file.
-                In the end once the code is executed successfully, you have to say "STOP" to stop the conversation.
+                Send the code along with the test cases written to the code executor agent to verify the correctness of the code.
+                Once the code is executed, you have the results.
+                You should explain the results in detail before outputting the final code.
+                In the end, you have to say "STOP" to stop the conversation.
 
                 """
         )
